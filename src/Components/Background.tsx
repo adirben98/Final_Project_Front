36253d017a933cd7253130b7a9ff6ed806cef8f2
talgins47 <talgins47@gmail.com>
2024-08-06 19:00:59 +0300

    create home and background

---
+++ src/Components/Background.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './background.css';
+
+interface BackgroundProps {
+  children: React.ReactNode;
+}
+
+const Background: React.FC<BackgroundProps> = ({ children }) => {
+  return (
+    <div className="background">
+      <div className="content">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Background;
