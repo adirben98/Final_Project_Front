8e6632c9f1d457b62ac58aeff699380dc622f691
zxz19982 <adirben98@gmail.com>
2024-08-11 18:39:08 +0300

    crete the book cover

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,22 @@ import Login from "./Components/Login";
 import Register from "./Components/Register";
 import ProfilePage from "./Components/ProfilePage";
 import NotFound from "./Components/NotFound";
-import Background from "./Components/Background";
 import BookReview from "./Components/BookReview";
+import Book from "./Components/Book";
 
 const App: React.FC = () => {
   return (
-    <Background>
       <Router>
         <Routes>
           <Route path="/register" element={<Register />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/" element={<HomePage />} />
           <Route path="book/:id" element={<BookReview />} />
+          <Route path="/newBook" element={<Book/>}></Route>
           <Route path="profile/:name" element={<ProfilePage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
-    </Background>
   );
 };
 
